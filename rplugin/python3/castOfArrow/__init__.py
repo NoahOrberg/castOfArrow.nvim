8a3b0621bb133d7bd39fa3d7a3503ca3e17112f2
NoahOrberg <noah.orberg.9999@gmail.com>
2017-06-27 23:43:05 +0900

    fix Character-code

--- rplugin/python3/castOfArrow/__init__.py
+++ rplugin/python3/castOfArrow/__init__.py
@@ -13,4 +13,4 @@ class castOfArrow(object):
     @neovim.command("CastSatanha")
     def castSatanha(self):
         satanha = ["hjklキー使えってこと!?師匠なのに!","こんな方向キー使う奴野放しにしてるなんて天界どうかしてるっ!!","矢印キーで移動するようとするなんて・・・悪っ!!", "どう!?矢印キーなんて最高に悪魔的な行為でしょっ!!", "矢印キーの形態はすべて掌握しているわ それもすべて・・・","上矢印とかkってなに!?"]
-        self.nvim.command("echo " + random.choice(satanha))
+        self.nvim.command("echo " + random.choice(satanha).decode('utf-8'))
